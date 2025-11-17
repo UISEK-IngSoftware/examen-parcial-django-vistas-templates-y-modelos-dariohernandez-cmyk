918a7c093a51e4bec2a64a1fb54d5d9d3204e36f
Dario Hernandez <dario.hernandez@uisek.edu.ec>
2025-11-17 00:31:22 -0500

    EXAMEN TERMINADO

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+    director_name = models.CharField(max_length=100)
+    genre = models.CharField(max_length=50)
+    synopsis = models.TextField()
+    image = models.ImageField(upload_to='movies/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
---
+++ movies/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.movie_list, name='movie_list'),             
+    path('<int:pk>/', views.movie_detail, name='movie_detail'), 
+    path('add/', views.movie_create, name='movie_create'),     
+    path('<int:pk>/edit/', views.movie_edit, name='movie_edit'), 
+]
